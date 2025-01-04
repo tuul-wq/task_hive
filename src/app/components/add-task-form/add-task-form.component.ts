ebdfc3dddd0958b0cfa3b7dfc625ccd6dbba5826
Yaroslav Grachev <tuulwq@gmail.com>
2025-01-04 19:43:01 +0300

    feat: basic structure

---
+++ src/app/components/add-task-form/add-task-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-task-form',
+  imports: [],
+  templateUrl: './add-task-form.component.html',
+  styleUrl: './add-task-form.component.scss'
+})
+export class AddTaskFormComponent {
+
+}
