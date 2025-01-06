b5b0b47ef832fb9ab48cc2098cb2293aac06775e
Yaroslav Grachev <tuulwq@gmail.com>
2025-01-06 22:55:11 +0300

    feat: styles and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { NotFound404Component } from './components/not-found-404/not-found-404.component';
-import { AppComponent } from './app.component';
+import { HiveComponent } from './pages/hive/hive.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
-  { path: '++', component: NotFound404Component },
+  { path: 'hive', component: HiveComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '', redirectTo: 'hive', pathMatch: 'full' },
+  { path: '**', component: NotFound404Component },
 ];
---
+++ src/app/pages/hive/hive.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hive-page',
+  imports: [],
+  templateUrl: './hive.component.html',
+  styleUrl: './hive.component.scss'
+})
+export class HiveComponent {
+
+}
