a42df99f5d4d1659c389e237868c4457d2ac05ef
Yaroslav Grachev <tuulwq@gmail.com>
2025-01-06 19:15:53 +0300

    feat: fonts and favicon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { NotFound404Component } from './components/not-found-404/not-found-404.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: '++', component: NotFound404Component },
+];
